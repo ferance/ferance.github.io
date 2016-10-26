a9577693953c8348bb711944b5bac6ef2cd1ef17
John Erber <johneemail05@gmail.com>
2016-10-26 16:49:25 -0400

    save

--- JohnDodge/JohnDodge.js
+++ JohnDodge/JohnDodge.js
@@ -1,4 +1,23 @@
 class Boot {
   preload() {
-    thid.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+  create() {
+    this.state.start("Load")
+  }
+}
+
+class Load {
+  preload() {
+    console.log("Loading...");
+  }
+  create() {
+    console.log("Loaded");
+  }
+}
 var game = new Phaser.Game(320,568);
+game.state.add("Boot",Boot);
+game.state.add("Load",Load);
+game.state.start("Boot");
