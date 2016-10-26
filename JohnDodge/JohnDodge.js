16c7d64633fa7234dc8910a2a42a94f427e8a1a5
John Erber <johneemail05@gmail.com>
2016-10-26 17:10:07 -0400

    save

--- JohnDodge/JohnDodge.js
+++ JohnDodge/JohnDodge.js
@@ -12,12 +12,23 @@ class Boot {
 class Load {
   preload() {
     console.log("Loading...");
+    this.load.image("bg","JohnAssets/background.jpeg")
   }
   create() {
     console.log("Loaded");
+    this.state.start("Play")
   }
 }
+
+class Play {
+  create() {
+    console.log("Entered Play State");
+    this.background = this.add.tileSprite(0,0,320,568,"bg");
+  }
+}
+
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",Boot);
 game.state.add("Load",Load);
+game.state.add("Play",Play);
 game.state.start("Boot");
