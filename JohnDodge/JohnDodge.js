6b49e1a9ebab022d17764a8810cdfdb538401343
John Erber <johneemail05@gmail.com>
2016-11-02 17:11:34 -0400

    save

--- JohnDodge/JohnDodge.js
+++ JohnDodge/JohnDodge.js
@@ -8,6 +8,16 @@ var C = {
     "height": 568,
     "xspeed": 0,
     "yspeed": 0,
+    "file": "JohnAssets/background.jpeg"
+  },
+  "p": {
+    "file": "JohnAssets/Duck.png",
+    "width": 46,
+    "height": 64,
+    "frame": 2
+  }
+}
+
 class Boot {
   preload() {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -22,7 +32,8 @@ class Boot {
 class Load {
   preload() {
     console.log("Loading...");
-    this.load.image("bg","JohnAssets/background.jpeg")
+    this.load.image("bg",C.bg.file)
+    this.load.spritesheet("player",C.p.file,C.p.width,C.p.height,C.p.frames)
   }
   create() {
     console.log("Loaded");
@@ -33,11 +44,15 @@ class Load {
 class Play {
   create() {
     console.log("Entered Play State");
-    this.background = this.add.tileSprite(0,0,320,568,"bg");
+    this.bg = this.add.tileSprite(0,0,C.bg.width,C.bg.height,"bg");
   }
 }
 
-var game = new Phaser.Game(320,568);
+function restart() {
+  game.state.start("Boot");
+}
+
+var game = new Phaser.Game(C.game.width,C.game.height);
 game.state.add("Boot",Boot);
 game.state.add("Load",Load);
 game.state.add("Play",Play);
